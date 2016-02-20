be28d7c2ae2a57d14cfdba93cb52d3e248d200ba
Victor Tsaran <vtsaran@yahoo.com>
2016-02-19 23:47:09 -0800

    Build script will now compile in the necessary 'swell' files. The dylib can now take advantages of the Windows functions such as GetTickCount, etc.

--- src/build_mac.sh
+++ src/build_mac.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-clang++ -dynamiclib -o reaper_osara.dylib -current_version 1.0 -compatibility_version 1.0 -fvisibility=hidden -undefined dynamic_lookup -stdlib=libc++ -std=c++11 -I ../include -I ../include/WDL -I ../include/WDL/wdl/swell reaper_osara.cpp -fobjc-arc -framework Appkit osxa11y.mm
+include_dir=../include
+wdl_dir=../include/WDL
+swell_dir=../include/WDL/WDL/swell
+clang++ -dynamiclib -o reaper_osara.dylib -current_version 1.0 -compatibility_version 1.0 -fvisibility=hidden -stdlib=libc++ -std=c++11 -I $include_dir -I $wdl_dir -I $swell_dir reaper_osara.cpp $swell_dir/swell.cpp $swell_dir/swell-ini.cpp $swell_dir/swell-dlg.mm $swell_dir/swell-gdi.mm $swell_dir/swell-kb.mm $swell_dir/swell-misc.mm $swell_dir/swell-miscdlg.mm $swell_dir/swell-menu.mm $swell_dir/swell-wnd.mm $swell_dir/swell-modstub.mm -framework Cocoa -framework Carbon -framework Appkit osxa11y.mm
