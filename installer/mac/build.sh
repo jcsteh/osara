b8e80bbe5a6a9a8c21a14c00b03fba0010f7102d
James Teh <jamie@nvaccess.org>
2016-05-27 20:45:20 +1000

    Mac dmg: Put copying.txt at top level instead of inside the hidden .data folder so it can actually be read.

--- installer/mac/build.sh
+++ installer/mac/build.sh
@@ -4,11 +4,12 @@ dmg=../osara_$version.dmg
 cd "`dirname \"$0\"`"
 rm -rf content/.data
 mkdir content/.data
-cd content/.data
+cd content
+cp ../../../copying.txt .
+cd .data
 cp ../../../../build/reaper_osara.dylib .
 cp ../../../../config/reaper-kb.ini OSARA.ReaperKeyMap
-cp ../../../../copying.txt .
 cd ../..
 rm -f $dmg
 hdiutil create -volname "OSARA $version" -srcfolder content $dmg
-rm -rf content/.data
+rm -rf content/copying.txt content/.data
