301e3dd9edc59082c571e0d3b15ba7b6516e26aa
AZSlow3 <admin@azslow.com>
2018-11-11 23:39:52 +0100

    Fix switching to plug-in window inside FX chain. (#181)
    Previously, this was quite broken.
    In addition:
    
    1. This has now been extended for floating FX windows.
    2. If there's a child control in the FX GUI which can be focused, focus that instead of the main window, as that is generally more useful.
    3. Avoid reporting of "CPU/PDC" label.

--- src/reaper_osara.cpp
+++ src/reaper_osara.cpp
@@ -1176,27 +1176,61 @@ void clickIoButton(MediaTrack* track, bool rightClick=false) {
 
 bool maybeSwitchToFxPluginWindow() {
 	HWND window = GetForegroundWindow();
-	char name[4];
+	char name[8];
 	if (GetWindowText(window, name, sizeof(name)) == 0)
 		return false;
-	if (strncmp(name, "FX: ", 4) != 0)
+	if (strncmp(name, "FX: ", 4) != 0 && // FX chain window
+		// floating FX window, for different plug-in types
+		strncmp(name, "DX: ", 4) != 0 &&
+		strncmp(name, "VST: ", 5) != 0 &&
+		strncmp(name, "VSTi: ", 6) != 0 &&
+		strncmp(name, "VST3: ", 6) != 0 &&
+		strncmp(name, "VST3i: ", 7) != 0
+	) {
 		return false;
-	// Descend.
-	if (!(window = GetWindow(window, GW_CHILD)))
+	}
+	// Descend. Observed as the first or as the last.
+	if (!(window = FindWindowExA(window, nullptr, "#32770", nullptr))) {
 		return false;
+	}
 	// This is a property page containing the plugin window among other things.
-	if (!(window = GetWindow(window, GW_HWNDLAST)))
-		return false;
-	// Descend.
+	// set property page name, to avoid CPU/PDC label audition after switching
+	if (GetWindowText(window, name, sizeof(name)) == 0) {
+		SetWindowText(window, " ");
+	}
+	// Descend. Observed as the first or as the last. 
+	// Can not just search, we do not know the class nor name.
 	if (!(window = GetWindow(window, GW_CHILD)))
 		return false;
-	// This should get the plugin window.
-	if (!(window = GetWindow(window, GW_HWNDLAST)))
+	char classname[16];
+	if (!GetClassName(window, classname, sizeof(classname))) {
 		return false;
-	HWND oldFocus = GetFocus();
-	SetFocus(window);
-	if (GetFocus() == oldFocus) // Focus didn't move
+	}
+	if (!strcmp(classname, "ComboBox")) {
+		// Plugin window should be the last.
+		if (!(window = GetWindow(window, GW_HWNDLAST))) {
+			return false;
+		}
+	} // else it is the first
+	// We have found plug-in window or its container
+	HWND plugin = window;
+	// if focus is already inside plug-in window, let F6 work as usual
+	HWND focus = GetFocus();
+	if ((focus == plugin) || (IsChild(plugin, focus))) {
 		return false;
+	}
+	// Try to focus the first child in Z order
+	HWND child;
+	while ((child = GetWindow(window, GW_CHILD))) {
+		window = child;
+	}
+	while (window) {
+		SetFocus(window);
+		if ((window == plugin) || (GetFocus() == window)) {
+			break; // success or the last possible attempt
+		}
+		window = GetParent(window);
+	}
 	return true;
 }
 
