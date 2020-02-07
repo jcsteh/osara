7420fe688282e26f4dfc2ca5e92d55880dd45e23
kyleman <kyleborah1234321@gmail.com>
2020-02-07 10:51:12 -0600

    Removed the need for the sudo command in the hdiutil commands.

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 scons
 if [ $? -eq 0 ]; then
-	sudo hdiutil mount ./installer/osara_unknown.dmg
+	hdiutil mount ./installer/osara_unknown.dmg
 	/Volumes/OSARA\ unknown/Install\ OSARA\ extension.command
-		sudo hdiutil unmount /Volumes/OSARA\ unknown
+	hdiutil unmount /Volumes/OSARA\ unknown
 fi
