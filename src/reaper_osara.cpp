dcd8e8f3b7041ea3fc813d63df3ae984ae6e6e62
Robbie Murray <rob@mur.org.uk>
2020-07-18 18:14:14 +0100

    Fix issue where Osara doesn't always find stretch markers

--- src/reaper_osara.cpp
+++ src/reaper_osara.cpp
@@ -783,16 +783,17 @@ int getStretchAtPos(MediaItem_Take* take, double pos, double itemStart,
 	// Stretch marker positions are relative to the start of the item and the
 	// take's play rate.
 	double posRel = (pos - itemStart) * playRate;
-	if (posRel < 0)
+	double posRelAdj = posRel-0.00003; // approximately 1 sample at 48000. 
+	if (posRelAdj < 0)
 		return -1;
-	int index = GetTakeStretchMarker(take, -1, &posRel, NULL);
+	int index = GetTakeStretchMarker(take, -1, &posRelAdj, NULL);
 	if (index < 0)
 		return -1;
 	double stretchRel;
 	// Get the real position; pos wasn't written.
 	GetTakeStretchMarker(take, index, &stretchRel, NULL);
-	if (stretchRel != posRel)
-		return -1; // Marker not right at pos.
+	if (abs(stretchRel - posRel)>0.00003)
+		return -1; // Marker not near to  pos.
 	return index;
 }
 
