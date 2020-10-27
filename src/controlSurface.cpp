3527048f905ddb8ecc421101bc6473fe589e64e0
James Teh <jamie@jantrid.net>
2020-10-27 14:24:28 +1000

    Report FX parameters changed by control surfaces, etc.

--- src/controlSurface.cpp
+++ src/controlSurface.cpp
@@ -44,6 +44,35 @@ class Surface: public IReaperControlSurface {
 		reportRepeat(repeat);
 	}
 
+	virtual int Extended(int call, void* parm1, void* parm2, void* parm3) override {
+		if (call == CSURF_EXT_SETFXPARAM) {
+			if (!this->shouldHandleChange()) {
+				return 0; // Unsupported.
+			}
+			auto track = (MediaTrack*)parm1;
+			int fx = *(int*)parm2 >> 16;
+			int param = *(int*)parm2 & 0xFFFF;
+			// parm3 is supposedly normalized value, but it seems to be incorrect in
+			// some cases.
+			double value = TrackFX_GetParam(track, fx, param, nullptr, nullptr);
+			ostringstream s;
+			char chunk[256];
+			TrackFX_GetFXName(track, fx, chunk, sizeof(chunk));
+			s << chunk << " ";
+			TrackFX_GetParamName(track, fx, param, chunk, sizeof(chunk));
+			s << chunk << " ";
+			TrackFX_FormatParamValue(track, fx, param, value, chunk,
+				sizeof(chunk));
+			if (chunk[0]) {
+				s << chunk;
+			} else {
+				s << value;
+			}
+			outputMessage(s);
+		}
+		return 0; // Unsupported.
+	}
+
 	private:
 	bool shouldHandleChange() {
 		DWORD now = GetTickCount();
