6e36090c107f2fdbe74cbc42762beab4610ddfc7
James Teh <jamie@jantrid.net>
2021-02-18 11:27:07 +1000

    Add ability to generate gettext translation template (.pot) with scons pot.

---
+++ makePot.py
@@ -0,0 +1,97 @@
+# OSARA: Open Source Accessibility for the REAPER Application
+# Utility to build translation (pot) template
+# Author: James Teh <jamie@jantrid.net>
+# Copyright 2021 James Teh
+# License: GNU General Public License version 2.0
+
+import re
+from collections import OrderedDict
+
+# Maps (context, msgid) to a dict of message data. We need this so we output
+# only one entry for each message.
+messages = OrderedDict()
+
+def makePot(target, source, env):
+	global messages
+	out = open(target[0].path, "wt", encoding="UTF-8")
+	out.write(
+r"""msgid ""
+msgstr ""
+"Content-Type: text/plain; charset=UTF-8\n"
+"Content-Transfer-Encoding: 8bit\n"
+
+"""
+	)
+	for s in source:
+		s = s.path
+		inp = open(s, "rt", encoding="UTF-8")
+		if s.endswith(".cpp"):
+			addCpp(inp)
+		elif s.endswith(".rc"):
+			addRc(inp)
+	for (context, msgid), data in messages.items():
+		for comment in data.get("comments", ()):
+			out.write('#. %s\n' % comment)
+		if context:
+			out.write('msgctxt "%s"\n' % context)
+		out.write('msgid "%s"\n' % data["msgid"])
+		out.write('msgstr ""\n')
+		out.write("\n")
+
+RE_TRANSLATORS_COMMENT = re.compile(r"^\s*// Translators: (.*)$")
+RE_COMMENT = re.compile(r"^\s*// (.*)$")
+inTranslatorsComment = False
+lastTranslatorsComment = []
+def handleTranslatorsComment(line):
+	global inTranslatorsComment, lastTranslatorsComment
+	m = RE_TRANSLATORS_COMMENT.match(line)
+	if m:
+		inTranslatorsComment = True
+		lastTranslatorsComment.append(m.group(1))
+		return True
+	if inTranslatorsComment:
+		m = RE_COMMENT.match(line)
+		if m:
+			lastTranslatorsComment.append(m.group(1))
+			return True
+		else:
+			inTranslatorsComment = False
+	return False
+
+def addMessage(data):
+	global messages, lastTranslatorsComment
+	key = (data.get("context"), data["msgid"])
+	data = messages.setdefault(key, data)
+	if lastTranslatorsComment:
+		comments = data.setdefault("comments", [])
+		comments.extend(lastTranslatorsComment)
+		lastTranslatorsComment = []
+
+RE_CPP_TRANSLATE = re.compile(r'\btranslate\("(?P<msgid>.*?)"\)')
+RE_CPP_TRANSLATE_CTXT = re.compile(r'\btranslate_ctxt\("(?P<context>.*?)",\s*"(?P<msgid>.*?)\)')
+def addCpp(input):
+	for line in input:
+		if handleTranslatorsComment(line):
+			continue
+		m = (RE_CPP_TRANSLATE.search(line) or
+			RE_CPP_TRANSLATE_CTXT.search(line))
+		if m:
+			addMessage(m.groupdict())
+
+RE_RC_TRANSLATE = re.compile(r'^\s*(?P<command>CAPTION|LTEXT|DEFPUSHBUTTON|GROUPBOX|CONTROL)\s+"(?P<msgid>.*?)"')
+def addRc(input):
+	context = None
+	for line in input:
+		if handleTranslatorsComment(line):
+			continue
+		m = RE_RC_TRANSLATE.match(line)
+		if m:
+			data = m.groupdict()
+			if data["command"] == "CAPTION":
+				context = data["msgid"]
+			if not context:
+				raise RuntimeError("No caption before messages")
+			if not data["msgid"]:
+				continue
+			data["context"] = context
+			addMessage(data)
