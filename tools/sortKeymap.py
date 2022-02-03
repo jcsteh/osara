a8403473698d41b9ef71524202c22a106192de02
Robbie Murray <rob@mur.org.uk>
2022-02-03 22:31:36 +0000

    Add tools/sortKeymap.py to hopefully make keymap diffs smaller

---
+++ tools/sortKeymap.py
@@ -0,0 +1,43 @@
+import re
+import sys
+
+regexs = {
+	'ACT': re.compile(r'(ACT) (\d+) (\d+) ("[0-9a-f]+") (".*") (.*)$'),
+	'SCR': re.compile(r'(SCR) (\d+) (\d+) ([^ ]+) (".*") (.*)$'),
+	'KEY': re.compile(r'(KEY) (\d+) (\d+) ([^ ]+) (\d+)$')
+}
+
+
+def parseLine(line):
+	for key,rx in regexs.items():
+		match = rx.match(line)
+		if(match):
+			return key,match
+	return None,None
+
+acts=[]
+scrs=[]
+keys=[]
+
+orig = open(sys.argv[1]).readlines()
+for line in orig:
+	key,match = parseLine(line)
+	if match:
+		if key == 'ACT':
+			acts.append(match)
+		elif key == 'SCR':
+			scrs.append(match)
+		elif key == 'KEY':
+			keys.append(match)
+
+from operator import methodcaller
+acts.sort(key = lambda m: (m.group(3), m.group(5)))
+scrs.sort(key = lambda m: (m.group(3), m.group(5)))
+keys.sort(key = lambda m: (m.group(5), m.group(4)))
+for match in (acts+scrs+keys):
+	if match:
+		out = ' '.join(match.groups())
+		print(out)
+	else:
+		raise Exception("failed to parse: "+out)
+		
