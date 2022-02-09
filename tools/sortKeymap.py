ad7a0b026f8ff981602b2bb3bc5f5305ea1d3cd1
Robbie Murray <rob@mur.org.uk>
2022-02-09 16:34:57 +0000

    improve sortKeymap.py

--- tools/sortKeymap.py
+++ tools/sortKeymap.py
@@ -1,5 +1,6 @@
-import re
 import sys
+import re
+import os
 
 regexs = {
 	'ACT': re.compile(r'(ACT) (\d+) (\d+) ("[0-9a-f]+") (".*") (.*)$'),
@@ -15,30 +16,42 @@ def parseLine(line):
 			return key,match
 	return None,None
 
-acts=[]
-scrs=[]
-keys=[]
+def sortKeyMap(lines):
+	acts=[]
+	scrs=[]
+	keys=[]
+	for line in lines:
+		key,match = parseLine(line)
+		if match:
+			if key == 'ACT':
+				acts.append(match)
+			elif key == 'SCR':
+				scrs.append(match)
+			elif key == 'KEY':
+				keys.append(match)
+		else:
+			raise Exception("Unable to parse line: "+line)
 
-orig = open(sys.argv[1]).readlines()
-for line in orig:
-	key,match = parseLine(line)
-	if match:
-		if key == 'ACT':
-			acts.append(match)
-		elif key == 'SCR':
-			scrs.append(match)
-		elif key == 'KEY':
-			keys.append(match)
+	acts.sort(key = lambda m: (m.group(3), m.group(5)))
+	scrs.sort(key = lambda m: (m.group(3), m.group(5)))
+	#sort keys by section, action id, modifiers and key.
+	keys.sort(key = lambda m: (m.group(5), m.group(4), m.group(2),m.group(3)))
+	return [match.group(0) for match in (acts+scrs+keys)]
 
-from operator import methodcaller
-acts.sort(key = lambda m: (m.group(3), m.group(5)))
-scrs.sort(key = lambda m: (m.group(3), m.group(5)))
-#sort keys by section, action id, modifiers and key.
-keys.sort(key = lambda m: (m.group(5), m.group(4), m.group(2),m.group(3)))
-for match in (acts+scrs+keys):
-	if match:
-		out = ' '.join(match.groups())
-		print(out)
-	else:
-		raise Exception("failed to parse: "+out)
-		
+def main():
+	fn = sys.argv[1]
+	orig = open(fn, encoding='utf-8').readlines()
+	os.replace(fn, fn+'.bak')
+	try:
+		sorted = sortKeyMap(orig)
+		with open(fn, 'x', encoding='utf-8') as outFile:
+			outFile.write('\n'.join(sorted))
+			outFile.write('\n')
+	except Exception as e:
+		os.replace(fn+'.bak', fn)
+		print(e)
+		return 1
+
+if __name__ == "__main__":
+	exitCode = main()
+	sys.exit(exitCode)
