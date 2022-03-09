c9204ec70b78688ac66eba519ec1385874283767
Robbie Murray <rob@mur.org.uk>
2022-03-09 10:14:18 +0000

    WIP

--- tools/kb2header.py
+++ tools/kb2header.py
@@ -2,14 +2,35 @@ import sortKeymap
 
 def buildKeyList(fn):
 	lines = open(fn, encoding='utf-8').readlines()
-	sections = {
-		0: [], 100:[], 102:[], 103:[],
+	numericActions = {
+		0:[], 100:[], 102:[], 103:[],
 		32060:[], 32061:[], 32062:[], 32063:[]}
-		for line in lines:
-			key, match = sortKeymap.parseLine(line)
-			if key != 'KEY':
-				continue
-			section = int(match.group('section'))
-			modifiers = int(match.group('modifiers'))
-			key = int(match.group('key'))
-			
+	customActions = {
+		0:[], 100:[], 102:[], 103:[],
+		32060:[], 32061:[], 32062:[], 32063:[]}
+	for line in lines:
+		key, match = sortKeymap.parseLine(line)
+		if key != 'KEY':
+			continue
+		section = int(match.group('section'))
+		modifiers = int(match.group('modifiers'))
+		key = int(match.group('key'))
+		id=match.group('actionId')
+		if id[0] == '_':
+			customActions[section].append((key, id, modifiers))
+		else:
+			numericActions[section].append((key, int(id), modifiers))
+	return (numericActions, customActions)
+
+def formatArray(scList, isCustom):
+	s = ""
+	for sc in scList:
+		if s:
+			s += ',\n\t'
+		if isCustom:
+			s += f'{{{sc[0]}, "{sc[1]}", {sc[2]}}}'
+		else:
+			s += f'{{{sc[0]}, {sc[1]}, {sc[2]}}}'
+	return s
+
+a,b = buildKeyList('../config/windows/reaper-kb.ini')
