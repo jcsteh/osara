c70fecbad5cde4ec504b748f701e063b671ff0af
James Teh <jamie@jantrid.net>
2022-08-19 21:56:48 +1000

    Don't disable UIA notifications for Zhengdu (ZDSR), as it now supports them. Fixes #780.

--- src/uia.cpp
+++ src/uia.cpp
@@ -231,24 +231,6 @@ bool shouldUseUiaNotifications() {
 		) {
 			return false;
 		}
-		// Now check for screen readers which don't have in-process dlls, so we have
-		// to search for processes.
-		PROCESSENTRY32 entry;
-		entry.dwSize = sizeof(PROCESSENTRY32);
-		HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-		if (!Process32First(snapshot, &entry)) {
-			CloseHandle(snapshot);
-			return true;
-		}
-		do {
-			if (
-				strcmp(entry.szExeFile, "ZDSRMain.exe") == 0 // Zhengdu
-			) {
-				CloseHandle(snapshot);
-				return false;
-			}
-		} while (Process32Next(snapshot, &entry));
-		CloseHandle(snapshot);
 		return true;
 	}();
 	return cachedResult;
