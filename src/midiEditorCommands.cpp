d92926f6eb1caa78f9c22e22bff80665981865ae
MatejGolian <11446675+MatejGolian@users.noreply.github.com>
2024-06-16 17:48:59 +0200

    Try to write a describeCC function

--- src/midiEditorCommands.cpp
+++ src/midiEditorCommands.cpp
@@ -2084,6 +2084,41 @@ void postMidiChangeZoom(int command) {
 	}
 }
 
+string describeCC(MidiControlChange cc, MediaItem_Take* take, bool select=true) {
+	string s;
+	s += formatCursorPosition() << " ";
+	if (cc.muted) {
+		s += translate("muted") << " ";
+	}
+	if (cc.message1 == 0xA0) {
+		// Translators: MIDI poly aftertouch. {note} will be replaced with the note
+		// name and {value} will be replaced with the aftertouch value; e.g.
+		// "Poly Aftertouch c sharp 4  96"
+		s += format(translate("Poly Aftertouch {note}  {value}"),
+			"note"_a=getMidiNoteName(take, cc.message2, cc.channel),
+			"value"_a=cc.message3);
+	} else if (cc.message1 == 0xB0) {
+		// Translators: A MIDI CC. {control} will be replaced with the control number and name. {value} will be replaced with the value of the control; e.g. "control 70 (Sound Variation), 64"
+		s += format(translate("Control {control}, {value}"),
+		"control"_a=getMidiControlName(take, cc.message2, cc.channel),
+		"value"_a=cc.message3);
+	} else if (cc.message1 == 0xC0) {
+		//Translators: a MIDI program number.  {} will be replaced with the program number; e.g. "Program 5"
+		s += format(translate("Program {}"), cc.message2);
+	} else if (cc.message1 == 0xD0) {
+		// Midi channel pressure. {} will be replaced with the pressure value; e.g. "Channel pressure 64"
+		s += format(translate("Channel pressure {}"), cc.message2);
+	} else if (cc.message1 == 0xE0) {
+		auto pitchBendValue = (cc.message3 << 7) | cc.message2;
+		// Translators: MIDI pitch bend.  {} will be replaced with the pitch bend value; e.g. "Pitch Bend 100"
+		s += format(translate("Pitch Bend {}"), pitchBendValue);
+	}
+	if (!select && !isCCSelected(take, cc.index)) {
+		s += " " << translate("unselected");
+	}
+	return s;
+}
+
 void cmdMidiInsertCC(Command* command) {
 	HWND editor = MIDIEditor_GetActive();
 	MediaItem_Take* take = MIDIEditor_GetTake(editor);
@@ -2101,29 +2136,4 @@ void cmdMidiInsertCC(Command* command) {
 	}
 	selectCC(take, cc.index);
 	fakeFocus = FOCUS_CC;
-	ostringstream s;
-	if (cc.message1 == 0xA0) {
-		// Translators: MIDI poly aftertouch. {note} will be replaced with the note
-		// name and {value} will be replaced with the aftertouch value; e.g.
-		// "Poly Aftertouch c sharp 4  96"
-		s << format(translate("Poly Aftertouch {note}  {value}"),
-			"note"_a=getMidiNoteName(take, cc.message2, cc.channel),
-			"value"_a=cc.message3);
-	} else if (cc.message1 == 0xB0) {
-		// Translators: A MIDI CC. {control} will be replaced with the control number and name. {value} will be replaced with the value of the control; e.g. "control 70 (Sound Variation), 64"
-		s << format(translate("Control {control}, {value}"),
-		"control"_a=getMidiControlName(take, cc.message2, cc.channel),
-		"value"_a=cc.message3);
-	} else if (cc.message1 == 0xC0) {
-		//Translators: a MIDI program number.  {} will be replaced with the program number; e.g. "Program 5"
-		s << format(translate("Program {}"), cc.message2);
-	} else if (cc.message1 == 0xD0) {
-		// Midi channel pressure. {} will be replaced with the pressure value; e.g. "Channel pressure 64"
-		s << format(translate("Channel pressure {}"), cc.message2);
-	} else if (cc.message1 == 0xE0) {
-		auto pitchBendValue = (cc.message3 << 7) | cc.message2;
-		// Translators: MIDI pitch bend.  {} will be replaced with the pitch bend value; e.g. "Pitch Bend 100"
-		s << format(translate("Pitch Bend {}"), pitchBendValue);
-	}
-	outputMessage(s);
 }
