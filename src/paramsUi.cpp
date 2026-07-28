52e2753adb79e7a7922c7b2245cc9b1414c21c05
James Teh <jamie@jantrid.net>
2026-07-29 00:15:20 +1000

    Parameters dialog: For send volume/pan/mute parameters, add a context menu option to show/hide envelope. (#1419)

--- src/paramsUi.cpp
+++ src/paramsUi.cpp
@@ -312,7 +312,7 @@ class ParamsDialog {
 	double val;
 	string valText;
 	HWND prevFocus;
-	bool isDestroying = false;
+	bool shouldAllowDeactivate = false;
 	bool suppressValueChangeReport = false;
 	CallLater valChangeLater;
 
@@ -511,7 +511,7 @@ class ParamsDialog {
 					return TRUE;
 				} else if (LOWORD(wParam) == IDCANCEL) {
 					dialog->saveWindowPos();
-					dialog->isDestroying = true;
+					dialog->shouldAllowDeactivate = true;
 					DestroyWindow(dialogHwnd);
 					delete dialog;
 					return TRUE;
@@ -519,12 +519,12 @@ class ParamsDialog {
 				break;
 			case WM_CLOSE:
 				dialog->saveWindowPos();
-				dialog->isDestroying = true;
+				dialog->shouldAllowDeactivate = true;
 				DestroyWindow(dialogHwnd);
 				delete dialog;
 				return TRUE;
 			case WM_ACTIVATE:
-				if (!dialog->isDestroying && LOWORD(wParam) == WA_INACTIVE) {
+				if (!dialog->shouldAllowDeactivate && LOWORD(wParam) == WA_INACTIVE) {
 					// If something steals focus, close the dialog. Otherwise, we won't
 					// unregister the key hook,  surface feedback won't report FX parameter
 					// changes and there will be a dialog left open the user can't get to
@@ -755,6 +755,11 @@ class ParamsDialog {
 		if (choice == -1) {
 			return; // Cancelled.
 		}
+		// Some options can cause the focus to move, even though we want focus to
+		// remain in this dialog.
+		HWND origFocus = GetFocus();
+		// Don't let the unwanted focus movement dismiss the dialog.
+		this->shouldAllowDeactivate = true;
 		Param::AfterOption after = options[choice].second();
 		if (after == Param::AfterOption::invalidateValues) {
 			this->onParamChange();
@@ -763,6 +768,12 @@ class ParamsDialog {
 			this->updateParamList();
 		} else if (after == Param::AfterOption::dismiss) {
 			SendMessage(this->dialog, WM_CLOSE, 0, 0);
+			return;
+		}
+		this->shouldAllowDeactivate = false;
+		if (GetFocus() != prevFocus) {
+			// Restore the focus whence it came.
+			SetFocus(origFocus);
 		}
 	}
 
@@ -1272,8 +1283,15 @@ class TrackSendParamProvider: public ReaperObjParamProvider {
 	}
 
 	Param::MoreOptions getMoreOptions() final {
+		Param::MoreOptions options;
+		if (this->getEnvelopeName()) {
+			options.push_back({
+				translate("Show/hide &envelope"),
+				[this] { return this->showHideEnvelope(); }
+			});
+		}
 		if (this->category == 0) {
-			return {
+			options.insert(options.end(), {
 				{
 					translate("Go to send destination track"),
 					[this] { return this->goToTargetTrack("P_DESTTRACK"); }
@@ -1282,10 +1300,9 @@ class TrackSendParamProvider: public ReaperObjParamProvider {
 					translate("Delete send"),
 					[this] { return this->remove(); }
 				},
-			};
-		}
-		if (this->category == -1) {
-			return {
+			});
+		} else if (this->category == -1) {
+			options.insert(options.end(), {
 				{
 					translate("Go to receive source track"),
 					[this] { return this->goToTargetTrack("P_SRCTRACK"); }
@@ -1294,14 +1311,16 @@ class TrackSendParamProvider: public ReaperObjParamProvider {
 					translate("Delete receive"),
 					[this] { return this->remove(); }
 				},
-			};
+			});
+		} else {
+			options.insert(options.end(), {
+				{
+					translate("Delete hardware output"),
+					[this] { return this->remove(); }
+				},
+			});
 		}
-		return {
-			{
-				translate("Delete hardware output"),
-				[this] { return this->remove(); }
-			},
-		};
+		return options;
 	}
 
 	private:
@@ -1315,6 +1334,27 @@ class TrackSendParamProvider: public ReaperObjParamProvider {
 	Param::AfterOption remove() {
 		RemoveTrackSend(this->track, this->category, this->index);
 		return Param::AfterOption::invalidateParams;
+	}
+
+	const char* getEnvelopeName() {
+		if (this->name == "D_VOL") {
+			return "P_ENV:<VOLENV";
+		}
+		if (this->name == "D_PAN") {
+			return "P_ENV:<PANENV";
+		}
+		if (this->name == "B_MUTE") {
+			return "P_ENV:<MUTEENV";
+		}
+		return nullptr;
+	}
+
+	Param::AfterOption showHideEnvelope() {
+		auto* env = (TrackEnvelope*)this->getSetValue(this->getEnvelopeName(),
+			nullptr);
+		SetCursorContext(2, env);
+		Main_OnCommand(40884, 0); // Envelope: Toggle hide/display selected envelope
+		return Param::AfterOption::nothing;
 	}
 
 	MediaTrack* track;
