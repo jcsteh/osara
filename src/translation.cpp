923092983bf9486e367dc1860f942872dbcc64d6
James Teh <jamie@jantrid.net>
2026-08-19 21:49:59 +1000

    Map Chinese (Simplified).ReaperLangPack to OSARA's zh_CN translation. Fixes #1445.

--- src/translation.cpp
+++ src/translation.cpp
@@ -24,6 +24,7 @@ map<string, string> REAPER_LANG_TO_CODE = {
 	{"pt-BR", "pt_BR"},
 	{"Reaper+SWS_CHSDOU", "zh_CN"},
 	{"REAPER_zh_CN_www.szzyyzz.com", "zh_CN"},
+	{"Chinese (Simplified)", "zh_CN"},
 	{"REAPER_SWS_french", "fr_FR"},
 	{"Reaper5965_fr_sws_wip", "fr_FR"},
 	{"REAPER_SWS_FRC", "fr_CA"},
